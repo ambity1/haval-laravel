4df5c2dd78dd572349302c56327c9e8f09e76ce4
lknightnet <lknightnet@gmail.com>
2024-05-22 13:32:36 +0500

    second

--- database/migrations/2024_05_22_072612_create_news_table.php
+++ database/migrations/2024_05_22_072612_create_news_table.php
@@ -10,7 +10,6 @@ return new class extends Migration {
         Schema::create('news', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('image');
             $table->text('content');
             $table->date('date');
         });
